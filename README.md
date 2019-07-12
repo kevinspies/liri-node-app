878b1fbbddd68ac4665b0233915fa15a42a9ae94
Kevin Spies <kevin.spies@gmail.com>
2019-07-12 00:46:42 -0400

    omdb response achieved

--- README.md
+++ README.md
@@ -1,9 +1,13 @@
 # liri-node-app
 a backend node command line application that can communicate with three API's to get useful information for fun events near you! Bands in town API, OMDB API, and Spotify API
 
-1. This app aims to allow a developer to
+1. This app aims to allow a developer, using the command line, to query three different API's - spotify, omdb, and 'bands in town' for information about a song (artist, song name, preview link of song from spotify, album), movie (title, year, rating, country produced in, language, plot, actors), or a concert (name of venue, location, event date) respectively.
+
+
+commands:
 
 concert command --> node liri.js concert-this <artist/band name here>
 spotify command --> node liri.js spotify-this-song '<song name here>'
 movie command --> node liri.js movie-this '<movie name here>'
 random command --> node liri.js do-what-it-says
+
--- liri.js
+++ liri.js
@@ -1,17 +1,64 @@
 require("dotenv").config();
+var Spotify = require('node-spotify-api');
+var axios = require("axios");
 var keys = require("./keys.js");
 var spotify = new Spotify(keys.spotify);
 
+// Grabbing the search type argument
+var command = process.argv[2];
+// Joining the remaining arguments since an actor or tv show name may contain spaces
+var term = process.argv.slice(3).join(" ");
+
+//if no command is provided
+if (!command) {
+    console.log("please read the readme to know how to enter a valid input!");
+}
+// By default, if no search term is provided, search for "Andy Griffith"
+if (!term) {
+    console.log("please enter a search term, i.e. Land Before Time");
+}
+console.log("command + term: " + command + " " + term);
+//---------------------------------------------------------------------------------------------------------------------
+
+
+// bands in town -->  https://rest.bandsintown.com/artists/replaceWithBandName/events?app_id=codingbootcamp
+
+
+if (command === "movie-this" && term) {//and if term is true aka they entered a search term
+
+    // omdb -->  https://www.omdbapi.com/?t=god+father&y=&plot=short&apikey=trilogy 
+    var queryUrl = "http://www.omdbapi.com/?t=" + term + "&y=&plot=short&apikey=trilogy";
+
+    axios.get(queryUrl).then(
+        function (response) {
+            console.log(response.data.plot);
+        })
+        .catch(function (error) {
+            if (error.response) {
+                // The request was made and the server responded with a status code
+                // that falls out of the range of 2xx
+                console.log("---------------Data---------------");
+                console.log(error.response.data);
+                console.log("---------------Status---------------");
+                console.log(error.response.status);
+                console.log("---------------Status---------------");
+                console.log(error.response.headers);
+            } else if (error.request) {
+                // The request was made but no response was received
+                // `error.request` is an object that comes back with details pertaining to the error that occurred.
+                console.log(error.request);
+            } else {
+                // Something happened in setting up the request that triggered an Error
+                console.log("Error", error.message);
+            }
+            console.log(error.config);
+        });
+
+}
 
 
 
 
-
-// bands in town -->   https://rest.bandsintown.com/artists/replaceWithBandName/events?app_id=codingbootcamp
-
-// omdb -->  https://www.omdbapi.com/?t=god+father&y=&plot=short&apikey=trilogy 
-
-// var Spotify = require('node-spotify-api');
 
 // var spotify = new Spotify({
 //     id: <your spotify client id>,
@@ -26,17 +73,20 @@ var spotify = new Spotify(keys.spotify);
 //         console.log(data); 
 // });
 
+if (process.argv[2]) {
+    var command = process.argv[2];
+}
 
 
-if (process.argv[2] === "concert this") {
+if (command === "concert this") {
 
 }
-if (process.argv[2] === "spotify-this-song") {
+if (command === "spotify-this-song") {
 
 }
-if (process.argv[2] === "movie-this") {
+if (command === "movie-this") {
 
 }
-if (process.argv[2] === "do-what-it-says") {
+if (command === "do-what-it-says") {
 
 }
